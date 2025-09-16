c1b3ec0a5d21daf045299518e8c2089e4ebe9c80
gustavoab94 <gustavoalvesabreu94@gmail.com>
2025-09-15 23:02:46 -0300

    feat: configure style guide

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -1,12 +1,18 @@
+import { Inter } from "next/font/google";
 import { Footer } from "../footer";
 import { Header } from "../header";
 
 type LayoutProps = {
   children: React.ReactNode;
 };
+
+const inter = Inter({ subsets: ["latin"] });
+
 export const Layout = ({ children }: LayoutProps) => {
   return (
-    <div className="relative flex min-h-full flex-col bg-gray-900 dark:bg-gray-900">
+    <div
+      className={`${inter.className} relative flex min-h-screen flex-col bg-gray-700`}
+    >
       <Header />
       <main className="flex-1 flex flex-col mb-12">{children}</main>
       <Footer />
